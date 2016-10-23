ff0239b64269b2e9a618be2a19f91da899d8e77d
pastukhov11 <Jacen1993git>
2016-10-24 01:52:15 +0300

    первая загрузка

---
+++ app/src/main/java/com/pustukhov/jacen/beeradviser/FindBeerActivity.java
@@ -0,0 +1,38 @@
+package com.pustukhov.jacen.beeradviser;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+
+public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    //Call when the user clicks the button
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+//Получить ссылку на Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+//Получить вариант, выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Получить рекомендации от класса BeerExpert
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+//Display the beers
+        brands.setText(brandsFormatted);
+    }
+}
